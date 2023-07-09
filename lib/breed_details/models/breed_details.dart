0b4f6ca204a157d8089fed4855017ceec35edd8b
Yuliia <yutadv@gmail.com>
2023-07-09 20:39:24 +0300

    repository_ok

--- lib/breed_details/models/breed_details.dart
+++ lib/breed_details/models/breed_details.dart
@@ -1,4 +1,4 @@
-import 'package:architecture_sample/network/responses/breed_details_response.dart';
+import '../../network/responses/breed_details_response.dart';
 
 class BreedDetails {
   const BreedDetails({
@@ -42,7 +42,7 @@ class BreedDetails {
     required this.referenceImageId,
   });
 
-  final BreedDetailsResponse weight;
+  final Weight weight;
   final String id;
   final String name;
   final String cfaUrl;
@@ -80,4 +80,75 @@ class BreedDetails {
   final String? wikipediaUrl;
   final int? hypoallergenic;
   final String? referenceImageId;
+
+
+  factory BreedDetails.fromBreedDetailsResponse(BreedDetailsResponse response) {
+    return BreedDetails(
+      weight: Weight(
+        imperial: response.weight.imperial,
+        metric: response.weight.metric,
+      ),
+      id: response.id,
+      name: response.name,
+      cfaUrl: response.cfaUrl,
+      vetstreetUrl: response.vetstreetUrl,
+      vcahospitalsUrl: response.vcahospitalsUrl,
+      temperament: response.temperament,
+      origin: response.origin,
+      countryCodes: response.countryCodes,
+      countryCode: response.countryCode,
+      description: response.description,
+      lifeSpan: response.lifeSpan,
+      indoor: response.indoor,
+      lap: response.lap,
+      adaptability: response.adaptability,
+      affectionLevel: response.affectionLevel,
+      childFriendly: response.childFriendly,
+      catFriendly: response.catFriendly,
+      dogFriendly: response.dogFriendly,
+      energyLevel: response.energyLevel,
+      grooming: response.grooming,
+      healthIssues: response.healthIssues,
+      intelligence: response.intelligence,
+      sheddingLevel: response.sheddingLevel,
+      socialNeeds: response.socialNeeds,
+      strangerFriendly: response.strangerFriendly,
+      vocalisation: response.vocalisation,
+      bidability: response.bidability,
+      experimental: response.experimental,
+      hairless: response.hairless,
+      natural: response.natural,
+      rare: response.rare,
+      rex: response.rex,
+      suppressedTail: response.suppressedTail,
+      shortLegs: response.shortLegs,
+      wikipediaUrl: response.wikipediaUrl,
+      hypoallergenic: response.hypoallergenic,
+      referenceImageId: response.referenceImageId,
+    );
+  }
+}
+
+extension BreedDetailsResponseExtensions on BreedDetailsResponse {
+  BreedDetails toBreedDetails() {
+    return BreedDetails.fromBreedDetailsResponse(this);
+  }
+}
+
+
+class Weight {
+  final String imperial;
+  final String metric;
+
+  Weight({
+    required this.imperial,
+    required this.metric,
+  });
+
+  factory Weight.fromJson(Map<String, dynamic> json) {
+    return Weight(
+      imperial: json['imperial'],
+      metric: json['metric'],
+    );
+  }
 }
--- lib/repositories/breeds_dateils_search_repository.dart
+++ lib/repositories/breeds_dateils_search_repository.dart
@@ -9,45 +9,6 @@ class BreedDetailsSearchRepository {
 
   Future<BreedDetails> loadBreedDetails(String breedId) async {
     final breedDetailsResponse = await _breedDetailsApiService.getBreedDetails(breedId);
-    return BreedDetails(
-      weight: breedDetailsResponse.weight,
-      id: breedDetailsResponse.id,
-      name: breedDetailsResponse.name,
-      cfaUrl: breedDetailsResponse.cfaUrl,
-      vetstreetUrl: breedDetailsResponse.vetstreetUrl,
-      vcahospitalsUrl: breedDetailsResponse.vcahospitalsUrl,
-      temperament: breedDetailsResponse.temperament,
-      origin: breedDetailsResponse.origin,
-      countryCodes: breedDetailsResponse.countryCodes,
-      countryCode: breedDetailsResponse.countryCode,
-      description: breedDetailsResponse.description,
-      lifeSpan: breedDetailsResponse.lifeSpan,
-      indoor: breedDetailsResponse.indoor,
-      lap: breedDetailsResponse.lap,
-      adaptability: breedDetailsResponse.adaptability,
-      affectionLevel: breedDetailsResponse.affectionLevel,
-      childFriendly: breedDetailsResponse.childFriendly,
-      catFriendly: breedDetailsResponse.catFriendly,
-      dogFriendly: breedDetailsResponse.dogFriendly,
-      energyLevel: breedDetailsResponse.energyLevel,
-      grooming: breedDetailsResponse.grooming,
-      healthIssues: breedDetailsResponse.healthIssues,
-      intelligence: breedDetailsResponse.intelligence,
-      sheddingLevel: breedDetailsResponse.sheddingLevel,
-      socialNeeds: breedDetailsResponse.socialNeeds,
-      strangerFriendly: breedDetailsResponse.strangerFriendly,
-      vocalisation: breedDetailsResponse.vocalisation,
-      bidability: breedDetailsResponse.bidability,
-      experimental: breedDetailsResponse.experimental,
-      hairless: breedDetailsResponse.hairless,
-      natural: breedDetailsResponse.natural,
-      rare: breedDetailsResponse.rare,
-      rex: breedDetailsResponse.rex,
-      suppressedTail: breedDetailsResponse.suppressedTail,
-      shortLegs: breedDetailsResponse.shortLegs,
-      wikipediaUrl: breedDetailsResponse.wikipediaUrl,
-      hypoallergenic: breedDetailsResponse.hypoallergenic,
-      referenceImageId: breedDetailsResponse.referenceImageId,
-    );
+    return breedDetailsResponse.toBreedDetails();
   }
 }
